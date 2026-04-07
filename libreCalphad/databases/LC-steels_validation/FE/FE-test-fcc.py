Fixed issue with two-state Cp calculation not being included in the fitting routine correctly. Updated the FE unary descriptions. There's still a discrepancy between calculated Cp using the segmented regression routine and using pycalphad from the resultant Gibbs energy parameters that I need to resolve.

libreCalphad/databases/LC-steels_validation/FE/FE-test-fcc.py
@@ -39,14 +39,6 @@ ax.set_ylabel("Isobaric Heat Capacity (J/mol-formula-K)")
 ax.legend()
 fig.tight_layout()
 plt.savefig(f"./CPM-CALC-FE-{phase[0]}.png")
-ax.set_xlim((0, 300))
-# ax.set_ylim((0, 30))
-plt.savefig(f"./CPM-CALC-FE-{phase[0]}-300K.png")
-ax.set_xlim((1e0, 1e2))
-ax.set_xscale("log")
-# ax.set_ylim((1e-3, 1e1))
-# ax.set_yscale("log")
-plt.savefig(f"./CPM-CALC-FE-{phase[0]}-0K.png")
 plt.close()
 
 # Enthalpy plotting
@@ -54,7 +46,7 @@ plt.close()
 query = (
     (where("phases") == phase)
     & (where("components") == components)
-    & (where("output") == "HM")
+    & (where("output") == "DH")
 )
 search_results = datasets.search(query)
 
@@ -63,15 +55,15 @@ calc_res = calculate(
     dbf, components, phase, T=(0.5, 2000, 2), P=101325, N=1, output="HM"
 )
 H298 = (
-    calculate(dbf, components, phase, T=298.15, P=101325, N=1, output="HM")
+    calculate(dbf, components, "BCC_A2", T=298.15, P=101325, N=1, output="HM")
     .HM.squeeze()
     .values
 )
-ax.plot(calc_res.T, calc_res.HM.squeeze())
+ax.plot(calc_res.T, calc_res.HM.squeeze() - H298)
 for result in search_results:
     ax.scatter(
         result["conditions"]["T"],
-        np.array(result["values"]).squeeze() + H298,
+        np.array(result["values"]).squeeze(),
         label=result["reference"],
     )
 ax.set_xlabel("Temperature (K)")
@@ -86,7 +78,7 @@ plt.close()
 query = (
     (where("phases") == phase)
     & (where("components") == components)
-    & (where("output") == "SM")
+    & (where("output") == "DS")
 )
 search_results = datasets.search(query)
 
@@ -95,15 +87,15 @@ calc_res = calculate(
     dbf, components, phase, T=(0.5, 2000, 2), P=101325, N=1, output="SM"
 )
 S298 = (
-    calculate(dbf, components, phase, T=298.15, P=101325, N=1, output="SM")
+    calculate(dbf, components, "BCC_A2", T=298.15, P=101325, N=1, output="SM")
     .SM.squeeze()
     .values
 )
-ax.plot(calc_res.T, calc_res.SM.squeeze())
+ax.plot(calc_res.T, calc_res.SM.squeeze() - S298)
 for result in search_results:
     ax.scatter(
         result["conditions"]["T"],
-        np.array(result["values"]).squeeze() + S298,
+        np.array(result["values"]).squeeze(),
         label=result["reference"],
     )
 ax.set_xlabel("Temperature (K)")