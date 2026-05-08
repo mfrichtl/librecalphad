38d4a447fdb8763ad2d7a6cbf8eabcae57a7f800
Matt Frichtl <4640334+mfrichtl@users.noreply.github.com>
2026-05-08 07:34:08 -0400

    Added tests for new energy functions.

--- libreCalphad/tests/test_energy.py
+++ libreCalphad/tests/test_energy.py
@@ -9,9 +9,10 @@ import symengine as se
 def test_symbolic_gibbs():
     T, a = se.symbols("T a")
     Cp_f = a * T
-    gibbs = en._symbolic_gibbs(100, [2], Cp_f, ret_expr=True)
+    temp_bounds = (0, 1000)
+    gibbs = en._symbolic_gibbs(100, [2], Cp_f, temp_bounds, ret_expr=True)
     assert np.isclose(gibbs.subs([T, a], [100, 2]), -10000)
-    gibbs = en._symbolic_gibbs(100, [2], Cp_f)
+    gibbs = en._symbolic_gibbs(100, [2], Cp_f, temp_bounds)
     assert np.isclose(gibbs, -10000)
-    gibbs = en._symbolic_gibbs([0, 100], [2], Cp_f)
+    gibbs = en._symbolic_gibbs([0, 100], [2], Cp_f, temp_bounds)
     assert np.isclose(np.sum(gibbs), -10000)
